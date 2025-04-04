0cb8bfb46aef863abcd26b8c125cbd46e8398640
Bryan Matthews <bmatthews@peninsulaconsulting.ca>
2025-04-04 14:25:13 -0700

    Add simple yvshell to appmain

--- app/AppInterface.cpp
+++ app/AppInterface.cpp
@@ -3,11 +3,26 @@
 
 #include "main.h"
 
-void initializeApp() {
+class MyShell : public YRShell {
+    protected:
+        virtual const char* shellClass( void) { return "MyShell"; }
+        virtual const char* mainFileName( ) { return "app/AppInterface.cpp"; }
+    public:
+        MyShell() {}
+        virtual ~MyShell() {}
+    };
 
+    MyShell yrShell;
+
+void initializeApp() {
+    BufferedSerial::STM32SerialError error = BSerial2.begin(115200);
+    assert_param(error == BufferedSerial::STM32SerialError::none);
+
+    BSerial2.init(yrShell.getInq(), yrShell.getOutq());
 }
 
 void updateApp() {
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-    HAL_Delay(500);
+    // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    // HAL_Delay(500);
+    Sliceable::sliceAll();
 }
---
+++ app/processorGlobal.h
@@ -0,0 +1,10 @@
+#ifndef PROCESSOR_GLOBAL_H
+#define PROCESSOR_GLOBAL_H
+
+#include "main.h"
+
+//#define ENABLE_SERIAL1
+#define ENABLE_SERIAL2
+//#define ENABLE_SERIAL3
+
+#endif
